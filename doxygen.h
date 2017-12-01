99625bf9f3d2b4f288bcdba32c91a14ab1b801e9
Taylor Holberton <taylorcholberton@gmail.com>
2017-12-01 16:44:58 -0500

    added Doxygen files

---
+++ doxygen.h
@@ -0,0 +1,12 @@
+/* NOTE : This header is not used by the source
+ * code, it is only used by doxygen. */
+
+/** @mainpage Swanson OS Documentation
+ * <br>
+ * Welcome to the documentation for the Swanson OS project. <br>
+ * <br>
+ * At the heart of the project is the kernel, which contains
+ * the file system structures, memory structures and more. See
+ * @ref kernel for more information.
+ *
+ * */
