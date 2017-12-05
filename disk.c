4f700c8b018945f2f4a6c88bcb914b339831adba
Taylor Holberton <taylorcholberton@gmail.com>
2017-12-05 18:16:03 -0500

    added new disk functions

--- disk.c
+++ disk.c
@@ -18,6 +18,43 @@
 
 #include "disk.h"
 
-void disk_init(struct disk *disk) {
+void
+disk_init(struct disk *disk) {
 	stream_init(&disk->stream);
+	disk->sector_size = 512;
+}
+
+void
+disk_set_sector_size(struct disk *disk,
+                     uint64_t sector_size) {
+	if (sector_size > 0)
+		disk->sector_size = sector_size;
+}
+
+uint64_t
+disk_read(struct disk *disk,
+          void *buf,
+          uint64_t sector_count) {
+
+	uint64_t read_count;
+	uint64_t buf_size;
+
+	buf_size = sector_count * disk->sector_size;
+
+	read_count = stream_read(&disk->stream, buf, buf_size);
+
+	read_count /= disk->sector_size;
+
+	return 0;
+}
+
+int
+disk_seek(struct disk *disk,
+          uint64_t sector_index) {
+
+	uint64_t offset;
+
+	offset = sector_index * disk->sector_size;
+
+	return stream_setpos(&disk->stream, offset);
 }
--- disk.h
+++ disk.h
@@ -38,6 +38,8 @@ extern "C" {
 struct disk {
 	/** The stream callbacks for the disk. */
 	struct stream stream;
+	/** The sector size of the disk. */
+	uint64_t sector_size;
 };
 
 /** Initializes a disk structure.
@@ -47,7 +49,51 @@ struct disk {
  * structure.
  * */
 
-void disk_init(struct disk *disk);
+void
+disk_init(struct disk *disk);
+
+/** Sets the sector size of the disk.
+ * While this may be any value, the
+ * traditional values are: 512 for older
+ * hard drives, 2048 for CD and DVDs and
+ * 4096 for newer hard drives that use
+ * advanced format.
+ * @param disk An initialized disk structure.
+ * @param sector_size The new sector size.
+ * */
+
+void
+disk_set_sector_size(struct disk *disk,
+                     uint64_t sector_size);
+
+/** Reads sectors from a disk.
+ * @param disk An initialized disk
+ * structure.
+ * @param buf The buffer to write the
+ * sectors to.
+ * @param sector_count The number of
+ * sectors to read.
+ * @returns The number of sectors that
+ * were read.
+ * */
+
+uint64_t
+disk_read(struct disk *disk,
+          void *buf,
+          uint64_t sector_count);
+
+/** Seek to a new sector.
+ * @param disk An initialized disk
+ * structure.
+ * @param sector_index The index of
+ * the sector to move to.
+ * @returns Zero on success, non-zero
+ * otherwise.
+ * */
+
+int
+disk_seek(struct disk *disk,
+          uint64_t sector_index);
 
 #ifdef __cplusplus
 } /* extern "C" { */
