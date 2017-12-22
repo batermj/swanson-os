8fb1335496e7a1de9361fdb272e9262a2e07340c
Taylor Holberton <taylorcholberton@gmail.com>
2017-12-21 22:06:32 -0500

    fixed redirection error

--- scripts/format-dir.sh
+++ scripts/format-dir.sh
@@ -4,9 +4,9 @@ set -e
 set -u
 
 for file in $1/*.c; do
-	clang-format "$file" >"$file"
+	clang-format -i "$file"
 done
 
 for file in $1/*.h; do
-	clang-format "$file" >"$file"
+	clang-format -i "$file"
 done
