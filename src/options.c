3ac5de7d5e167f137d3ab3196b43f64642ac04f4
Taylor Holberton <taylorcholberton@gmail.com>
2017-12-22 19:49:45 -0500

    added options structure

---
+++ src/options.c
@@ -0,0 +1,104 @@
+/* Copyright (C) 2017 Taylor Holberton
+ *
+ * This file is part of Swanson.
+ *
+ * Swanson is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Swanson is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Swanson.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "options.h"
+
+#include "fdisk.h"
+
+#include <stdlib.h>
+
+void
+options_init(struct options *options) {
+	options->memory = 1 * 1024 * 1024;
+	options->disk_array = NULL;
+	options->disk_count = 0;
+}
+
+void
+options_free(struct options *options) {
+
+	unsigned long int i;
+
+	for (i = 0; i < options->disk_count; i++)
+		fdisk_close(&options->disk_array[i]);
+
+	free(options->disk_array);
+
+	options->disk_array = NULL;
+	options->disk_count = 0;
+}
+
+int
+options_add_disk(struct options *options,
+                 const char *disk_path) {
+
+	struct fdisk *disk;
+	struct fdisk *disk_array;
+	size_t disk_count;
+	size_t disk_array_size;
+
+	disk_array = options->disk_array;
+
+	disk_count = options->disk_count + 1;
+
+	disk_array_size = 0;
+	disk_array_size += sizeof(options->disk_array[0]);
+	disk_array_size *= disk_count;
+
+	disk_array = realloc(disk_array, disk_array_size);
+	if (disk_array == NULL)
+		return -1;
+
+	disk = &disk_array[disk_count - 1];
+
+	fdisk_init(disk);
+
+	fdisk_open(disk, disk_path, "r+");
+
+	options->disk_array = disk_array;
+	options->disk_count = disk_count;
+
+	return 0;
+}
+
+struct disk *
+options_get_disk(struct options *options,
+                 unsigned long int index) {
+	if (index >= options->disk_count) {
+		return NULL;
+	} else {
+		return fdisk_to_disk(&options->disk_array[index]);
+	}
+}
+
+unsigned long int
+options_get_disk_count(const struct options *options) {
+	return options->disk_count;
+}
+
+int
+options_set_memory(struct options *options,
+                   unsigned long int memory) {
+
+	if (memory == 0)
+		return -1;
+
+	options->memory = memory;
+
+	return 0;
+}
---
+++ src/options.h
@@ -0,0 +1,120 @@
+/* Copyright (C) 2017 Taylor Holberton
+ *
+ * This file is part of Swanson.
+ *
+ * Swanson is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Swanson is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Swanson.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file */
+
+#ifndef SWANSON_OPTIONS_H
+#define SWANSON_OPTIONS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct fdisk;
+
+/** Runtime options for the kernel.
+ * */
+
+struct options {
+	/** The number of bytes that the kernel may use. */
+	unsigned long int memory;
+	/** An array of files that may be used as disks. */
+	struct fdisk *disk_array;
+	/** The number of disks in the disk array. */
+	unsigned long int disk_count;
+};
+
+/** Initializes an options structure.
+ * @param options An uninitialized options structure.
+ * */
+
+void
+options_init(struct options *options);
+
+/** Releases resources allocated by the options
+ * structure.
+ * */
+
+void
+options_free(struct options *options);
+
+/** Adds a disk file to the options.
+ * @param options An initialized options structure.
+ * @param disk_path The path to the disk file.
+ * @returns Zero on success, a negative number on failure.
+ * */
+
+int
+options_add_disk(struct options *options,
+                 const char *disk_path);
+
+/** Gets a disk from the disk array in
+ * the options structure.
+ * @param options An initialized options
+ * structure.
+ * @param index The index of the disk
+ * within the disk array.
+ * @returns A pointer to the disk, from
+ * the disk array. If the index is out of
+ * bounds, this is NULL.
+ * @see options_add_disk
+ * @see options_get_disk_count
+ * */
+
+struct disk *
+options_get_disk(struct options *options,
+                 unsigned long int index);
+
+/** Returns the number of disks in the disk array.
+ * @param options An initialized options structure.
+ * @returns The number of disks within the disk array.
+ * @see options_get_disk
+ * @see options_add_disk
+ * */
+
+unsigned long int
+options_get_disk_count(const struct options *options);
+
+/** Parses an amount of memory in human-readable
+ * format.
+ * @param options An initialized options structure.
+ * @param memory The string containing the human-readable
+ * memory specification.
+ * @returns Zero on success, non-zero on failure.
+ * */
+
+int
+options_parse_memory(struct options *options,
+                     const char *memory);
+
+/** Sets the amount of memory to use by the kernel.
+ * @param options An initialized options structure.
+ * @param memory The amount of memory to assign the kernel.
+ * This parameter must be greater than zero.
+ * @returns Zero on success, non-zero on failure.
+ * */
+
+int
+options_set_memory(struct options *options,
+                   unsigned long int memory);
+
+#ifdef __cplusplus
+} /* extern "C" { */
+#endif
+
+#endif /* SWANSON_OPTIONS_H */
