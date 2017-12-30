305fb8d8c14b0373f1ed3b6a7a65d0d021e750d5
Taylor Holberton <taylorcholberton@gmail.com>
2017-12-30 10:53:24 -0500

    added return code

--- src/fs/fat/fat-format.c
+++ src/fs/fat/fat-format.c
@@ -1,3 +1,21 @@
+/* Copyright (C) 2017 Taylor Holberton
+ *
+ * This file is part of Swanson.
+ *
+ * Swanson is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Swanson is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Swanson.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -37,8 +55,9 @@ create_zero_img(const char *imagepath,
 	return EXIT_SUCCESS;
 }
 
-int main(int argc, const char **argv)
-{
+int
+main(int argc, const char **argv) {
+
 	const char *diskpath;
 	enum fat_error error;
 	struct fat_disk *disk;
@@ -90,4 +109,6 @@ int main(int argc, const char **argv)
 	fl_shutdown();
 
 	fat_diskfile_done(&diskfile);
+
+	return EXIT_SUCCESS;
 }
