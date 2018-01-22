400baaf4f5ddd228fe0d22168dc8e5ecc8ae12b1
Taylor Holberton <taylorcholberton@gmail.com>
2018-01-21 22:19:26 -0500

    ported kernel to c++

---
+++ include/swanson/kernel.hpp
@@ -0,0 +1,78 @@
+// Copyright (C) 2017 Taylor Holberton
+// 
+//  This file is part of Swanson.
+// 
+//  Swanson is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  Swanson is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with Swanson.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef SWANSON_KERNEL_HPP
+#define SWANSON_KERNEL_HPP
+
+#include "fs/any.h"
+#include "fs/ramfs/fs.h"
+#include "memmap.h"
+#include "partition.h"
+#include "process.h"
+#include "sstream.h"
+
+#include <vector>
+
+#include <cstdint>
+
+namespace swanson {
+
+/// The code returned from the kernel entry point.
+/// Indicates whether or not the kernel ran without a panic.
+/// @see Kernel::Main
+enum class ExitCode {
+	/// Kernel exited without a panic.
+	Success,
+	/// Kernel exited after a panic.
+	Failure
+};
+
+/// The kernel, encapsulated into a single
+/// class. There are no global variables, just
+/// the structures found in this class.
+class Kernel final {
+	/// The kernel's memory map.
+	memmap mmap;
+	/// The disks known by the kernel.
+	std::vector<disk*> disks;
+	/// The partition containing the
+	// operating system.
+	partition root_partition;
+	/// The root file system.
+	any_fs root_fs;
+	/// Initial ramfs file system.
+	ramfs initramfs;
+	/// The root process.
+	process init;
+public:
+	/// Default constructor.
+	Kernel() noexcept;
+	/// Default deconstructor.
+	~Kernel();
+	/// Adds a disk to the kernel.
+	void AddDisk(disk *disk);
+	/// Adds memory that may be used by the kernel.
+	void AddMemory(void *addr, uintmax_t size);
+	/// Loads the initial ramdisk.
+	void LoadInitRamfs(const void *addr, uintmax_t size);
+	/// The kernel's entry point.
+	ExitCode Main();
+};
+
+} // namespace swanson
+
+#endif /* SWANSON_KERNEL_HPP */
---
+++ src/kernel.cpp
@@ -0,0 +1,86 @@
+/* Copyright (C) 2017 Taylor Holberton
+ *
+ * This file is part of Swanson.
+ *
+ * Swanson is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Swanson is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Swanson.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <swanson/kernel.hpp>
+
+#include "debug.h"
+#include "disk.h"
+#include "gpt.h"
+#include "rstream.h"
+
+#include <cstdlib>
+#include <cstring>
+
+namespace swanson {
+
+Kernel::Kernel() noexcept {
+	any_fs_init(&root_fs);
+	memmap_init(&mmap);
+	partition_init(&root_partition);
+	ramfs_init(&initramfs);
+	process_init(&init);
+}
+
+Kernel::~Kernel() {
+	ramfs_free(&initramfs);
+}
+
+void Kernel::AddDisk(disk *disk) {
+	disks.push_back(disk);
+}
+
+void Kernel::AddMemory(void *addr, uintmax_t size) {
+
+	auto error = memmap_add(&mmap, addr, size);
+	if (error != MEMMAP_ERROR_NONE) {
+		/* TODO : error message. */
+	}
+}
+
+void Kernel::LoadInitRamfs(const void *buf, uintmax_t buf_size) {
+
+	struct rstream rstream;
+	struct stream *stream;
+
+	/** The 'rstream' represents a stream that
+	 * contains the initramfs serialized data.
+	 * */
+
+	rstream_init(&rstream);
+
+	rstream_setbuf(&rstream, buf, buf_size);
+
+	stream = rstream_to_stream(&rstream);
+
+	ramfs_decode(&initramfs, stream);
+}
+
+ExitCode Kernel::Main() {
+
+	struct ramfs_file *init;
+
+	init = ramfs_open_file(&initramfs, "/init");
+	if (init == NULL) {
+		debug("Failed to open 'init'.\n");
+		return ExitCode::Failure;
+	}
+
+	return ExitCode::Success;
+}
+
+} // namespace swanson
