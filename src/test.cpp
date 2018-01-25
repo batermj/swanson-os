9626a794f7043dfb5ddfd8dff4ba7cc670181d12
Taylor Holberton <taylorcholberton@gmail.com>
2018-01-24 19:51:31 -0500

    added CPU test to main program

--- src/test.cpp
+++ src/test.cpp
@@ -18,20 +18,38 @@
 
 #include "test.hpp"
 
+#include "cpu-test.hpp"
 #include "elf-test.hpp"
 
-#include "cpu-test.h"
 #include "crc32-test.h"
 #include "gpt-test.h"
 #include "memmap-test.h"
 #include "options-test.h"
 #include "path-test.h"
 
-#include <stdlib.h>
+#include <swanson/exception.hpp>
+#include <swanson/segfault.hpp>
 
-int
-main(void) {
-	swanson::tests::RunTests();
+#include <iostream>
+#include <iomanip>
+
+#include <cstdlib>
+
+int main() {
+
+	try {
+		swanson::tests::RunTests();
+	} catch (const swanson::Segfault &segfault) {
+		std::cerr << "Segmentation fault occured at 0x";
+		std::cerr << std::hex << std::setfill('0') << std::setw(8);
+		std::cerr << segfault.GetAddress();
+		std::cerr << std::endl;
+		return EXIT_FAILURE;
+	} catch (const swanson::Exception &exception) {
+		std::cerr << exception.What() << std::endl;
+		return EXIT_FAILURE;
+	}
+
 	return EXIT_SUCCESS;
 }
 
@@ -39,9 +57,9 @@ namespace swanson::tests {
 
 void RunTests() {
 	// C++ tests
+	TestCPU();
 	TestELF();
 	// Standard C tests
-	cpu_test();
 	crc32_test();
 	gpt_test();
 	memmap_test();
