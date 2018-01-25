13068fc239a8b8363e958a9fbf1bb2ca87891842
Taylor Holberton <taylorcholberton@gmail.com>
2018-01-24 20:10:03 -0500

    Added missing prototypes for memory operations

--- include/swanson/memory-section.hpp
+++ include/swanson/memory-section.hpp
@@ -31,14 +31,48 @@ class MemorySection final {
 	/// The virtual address of the
 	/// memory section.
 	uint32_t address;
+	/// Whether or not read operations
+	/// are allowed in this section.
+	bool readPermission;
+	/// Whether or not write operations
+	/// are allowed in this section.
+	bool writePermission;
+	/// Whether or not this section
+	/// of memory is executable.
+	bool executePermission;
 	/// The bytes of memory associated
 	/// with the memory section.
 	std::vector<unsigned char> bytes;
 public:
 	/// Default constructor.
-	MemorySection() noexcept : address(0x00) { }
+	MemorySection() noexcept : address(0x00),
+	                           readPermission(true),
+	                           writePermission(true),
+	                           executePermission(false) { }
 	/// Default deconstructor.
 	~MemorySection() { }
+	/// Indicate whether or not read
+	/// operations may occur at this section.
+	/// @param state True if read operations are
+	/// allowed, false if they are not.
+	void AllowRead(bool state) { readPermission = state; }
+	/// Indicate whether or not write
+	/// operations may occur at this section.
+	/// @param state True if write operations are
+	/// allowed, false if they are not.
+	void AllowWrite(bool state) { writePermission = state; }
+	/// Indicate whether or not execute
+	/// permissions may occur at this section.
+	/// @param state True if memory may be executed
+	/// at this section, false if it can not be.
+	void AllowExecute(bool state) { executePermission = state; }
+	/// Copy data to the memory section.
+	/// @param data The data to copy to the section.
+	/// @param size The number of bytes to copy.
+	void CopyData(const void *data, uint32_t size);
+	/// Copy data to the memory section.
+	/// @param bytes_ The data to copy to the section.
+	void CopyData(const std::vector<unsigned char> &bytes_) { bytes = bytes_; }
 	/// Determine if an address exists
 	/// within this memory section.
 	/// @param addr The address to check for.
@@ -46,11 +80,51 @@ public:
 	/// exists in the section, false if
 	/// it does not.
 	bool Exists(uint32_t addr) const noexcept;
-	/// Read 32 bits from memory.
+	/// Read a 32 bit value from memory.
 	/// @param addr The address to read from.
 	/// @returns The 32-bit value at the
 	/// specified address.
 	uint32_t Read32(uint32_t addr) const;
+	/// Read a 16 bit value from memory.
+	/// @param addr The address to read from.
+	/// @returns The 16-bit value at the
+	/// specified address.
+	uint16_t Read16(uint32_t addr) const;
+	/// Read an 8 bit value from memory.
+	/// @param addr The address to read from.
+	/// @returns The 8-bit value at the
+	/// specified address.
+	uint8_t Read8(uint32_t addr) const;
+	/// Fetch a 32-bit component of an instruction.
+	/// The section must have execute permission for this
+	/// function to work without throwing an exception.
+	/// @param addr The address of the instruction component.
+	/// @returns The 32-bit component of the instruction.
+	uint32_t Exec32(uint32_t addr) const;
+	/// Fetch a 16-bit component of an instruction.
+	/// The section must have execute permission for this
+	/// function to work without throwing an exception.
+	/// @param addr The address of the instruction component.
+	/// @returns The 16-bit component of the instruction.
+	uint16_t Exec16(uint32_t addr) const;
+	/// Write a 32-bit value to the memory section.
+	/// The section must have write permission for this
+	/// function to work without causing an exception.
+	/// @param addr The address to write the value at.
+	/// @param value The value to write to the section.
+	void Write32(uint32_t addr, uint32_t value);
+	/// Write a 16-bit value to the memory section.
+	/// The section must have write permission for this
+	/// function to work without causing an exception.
+	/// @param addr The address to write the value at.
+	/// @param value The value to write to the section.
+	void Write16(uint32_t addr, uint16_t value);
+	/// Write an 8-bit value to the memory section.
+	/// The section must have write permission for this
+	/// function to work without causing an exception.
+	/// @param addr The address to write the value at.
+	/// @param value The value to write to the section.
+	void Write8(uint32_t addr, uint8_t value);
 	/// Resize the section of memory.
 	/// Care should be taken that it
 	/// does not overlap with the other
